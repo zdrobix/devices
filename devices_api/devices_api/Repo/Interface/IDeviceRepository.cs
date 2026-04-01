fbdd525a005e2f1a87fb56a1b9d8d2bd4ec3a995
zdrobix <zdroba88@gmail.com>
2026-04-01 15:44:11 +0300

    Added repository interfaces.

---
+++ devices_api/devices_api/Repo/Interface/IDeviceRepository.cs
@@ -0,0 +1,13 @@
+﻿using devices_api.Models.Domain;
+
+namespace devices_api.Repo.Interface
+{
+    public interface IDeviceRepository
+    {
+		Task<Device> CreateAsync(Device Device);
+        Task<IEnumerable<Device>> GetAllAsync();
+        Task<Device?> GetById(int id);
+        Task<Device?> UpdateAsync(Device device);
+        Task<Device> DeleteAsync(int id);
+    }
+}
---
+++ devices_api/devices_api/Repo/Interface/IUserRepository.cs
@@ -0,0 +1,14 @@
+﻿using devices_api.Models.Domain;
+
+namespace devices_api.Repo.Interface
+{
+    public interface IUserRepository
+    {
+        Task<User> CreateAsync(User user);
+        Task<IEnumerable<User>> GetAllAsync();
+        Task<User?> GetById(int id);
+        Task<User?> UpdateAsync(User user);
+        Task<User> DeleteAsync(int id);
+        Task<User?> GetByName(string name);
+    }
+}
