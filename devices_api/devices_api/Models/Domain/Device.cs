61f22c385a44e71e08b2316a14e1817da887ad17
zdrobix <zdroba88@gmail.com>
2026-04-01 15:43:52 +0300

    Added domain files: Entity, User, Device.

---
+++ devices_api/devices_api/Models/Domain/Device.cs
@@ -0,0 +1,54 @@
+﻿using System.Diagnostics;
+using System.Runtime.Intrinsics.Arm;
+
+namespace devices_api.Models.Domain
+{
+    public class Device : Entity<int>
+    {
+        public string Name { get; set; }
+        public string Manufacturer{ get; set; }
+        public string Type{ get; set; }
+        public string OperatingSystem{ get; set; }
+        public string? OperatingSystemVersion{ get; set; }
+        public string? Processor{ get; set; }
+        public string? RAM{ get; set; }
+        public string? Description{ get; set; }
+        public User? UsedBy { get; set; }
+
+        public Device(
+            string name, 
+            string manufacturer, 
+            string type, 
+            string operatingSystem, 
+            string operatingSystemVersion, 
+            string processor, 
+            string ram, 
+            string description,
+            User? usedBy)
+        {
+            Name = name;
+            Manufacturer = manufacturer;
+            Type = type;
+            OperatingSystem = operatingSystem;
+            OperatingSystemVersion = operatingSystemVersion;
+            Processor = processor;
+            RAM = ram;
+            Description = description;
+            UsedBy = usedBy!;
+        }
+
+        public Device()
+        {
+            Name = "";
+            Manufacturer = "";
+            Type = "";
+            OperatingSystem = "";
+            OperatingSystemVersion = "";
+            Processor = "";
+            RAM = "";
+            Description = "";
+            UsedBy = null!;
+        }
+
+    }
+}
