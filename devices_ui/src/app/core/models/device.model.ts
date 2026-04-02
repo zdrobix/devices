622cea4b148867a8767ba38558b081aa1dea986a
zdrobix <zdroba88@gmail.com>
2026-04-02 14:18:59 +0300

    Added models for user&device.

---
+++ devices_ui/src/app/core/models/device.model.ts
@@ -0,0 +1,14 @@
+import { User } from "./user.model";
+
+export interface Device {
+    id: number,
+    name: string,
+    manufacturer: string,
+    type: string,
+    operatingSystem: string,
+    operatingSystemVersion: string,
+    processor: string,
+    ram: string,
+    description: string,
+    usedBy: User | null
+}
