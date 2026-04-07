d0b82ac153c8caca9bee0dc69d43e6b2c31ef16a
zdrobix <zdroba88@gmail.com>
2026-04-07 15:33:31 +0300

    Add logout in navbar, add device router link.

--- devices_ui/src/app/core/components/navbar/navbar.component.ts
+++ devices_ui/src/app/core/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../services/user/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-
-}
+  private userService = inject(UserService);
+  logout() : void {
+    console.log('Logging out...');
+    this.userService.logout();
+    console.log('Logged out.');
+    window.location.reload();
+  }
+} 
