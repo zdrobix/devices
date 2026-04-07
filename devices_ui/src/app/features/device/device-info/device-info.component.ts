c725bdd4d230fb5dd530cc4c6188b5142a9f8f16
zdrobix <zdroba88@gmail.com>
2026-04-07 15:36:08 +0300

    Device info empty component.

---
+++ devices_ui/src/app/features/device/device-info/device-info.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-device-info',
+  templateUrl: './device-info.component.html',
+  styleUrls: ['./device-info.component.css']
+})
+export class DeviceInfoComponent {
+
+}
