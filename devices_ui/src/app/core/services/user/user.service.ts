27a8390bc365f05f6a0d894669cadd11e47b3a3d
zdrobix <zdroba88@gmail.com>
2026-04-07 12:17:22 +0300

    Implemented login logic in user service.

---
+++ devices_ui/src/app/core/services/user/user.service.ts
@@ -0,0 +1,50 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable, tap } from 'rxjs';
+import { User } from '../../models/user.model';
+import { LoginRequest } from '../../models/login-request.mode';
+import { environment } from 'src/environment/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  private currentUserSubject = new BehaviorSubject<User | null>(this.getUserFromSession());
+  public user$: Observable<User | null> = this.currentUserSubject.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  setLoggedInUser(user: User) {
+    sessionStorage.setItem('user', JSON.stringify(user));
+    this.currentUserSubject.next(user);
+  }
+
+  getLoggedInUser(): Observable<User | null> {
+    return this.user$;
+  }
+
+  login (request: LoginRequest) : Observable<{token: string, user: User}> {
+    return this.http.post<{token: string, user: User}>(`${environment.apiBaseUrl}/api/user/login`, request)
+      .pipe(
+        tap((response: { token: string; user: User; }) => {
+          localStorage.setItem('jwt_token', response.token);
+          this.setLoggedInUser(response.user);
+        })
+      );
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwt_token');
+    sessionStorage.removeItem('user');
+    this.currentUserSubject.next(null);
+  }
+
+  /*
+  Returns the user stored in the browser's memory, otherwise null.
+  */
+  private getUserFromSession(): User | null {
+    const userJson = sessionStorage.getItem('user');
+    return userJson ? JSON.parse(userJson) : null;
+  }
+}
