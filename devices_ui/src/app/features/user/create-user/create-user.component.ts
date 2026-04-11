287be9998ff76ff6a145bafbcef72cb6f8d48185
zdrobix <zdroba88@gmail.com>
2026-04-11 11:50:36 +0300

    Implemented sign up functionality.

--- devices_ui/src/app/features/user/create-user/create-user.component.ts
+++ devices_ui/src/app/features/user/create-user/create-user.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { AddUserRequest } from 'src/app/core/models/add-user-request.model';
+import { LoginRequest } from 'src/app/core/models/login-request.mode';
+import { UserService } from 'src/app/core/services/user/user.service';
 
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
   styleUrls: ['./create-user.component.css']
 })
-export class CreateUserComponent {
+export class CreateUserComponent implements OnInit, OnDestroy  {
+  model: AddUserRequest;
+  private createUserSubscription?: Subscription;
 
+  constructor(private userService: UserService,
+    private router: Router
+  ) {
+    this.model = {
+      name: '',
+      role: '',
+      location: '',
+      password: ''
+    };
+  }
+
+  ngOnInit(): void {
+  }
+
+  onFormSubmit() {
+    if (!this.model)
+      return; 
+    this.createUserSubscription = this.userService.createUser(this.model).subscribe({
+      next: () => {
+        this.router.navigate(['/devices']);
+      },
+      error: (error) => {}
+      });
+  }
+
+  ngOnDestroy(): void {
+    this.createUserSubscription?.unsubscribe();
+  }
 }
